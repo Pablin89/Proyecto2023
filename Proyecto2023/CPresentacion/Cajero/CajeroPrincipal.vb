45f7fa4ca6c0f784f036a4baf3718bb191d58f15
Pablin89 <113114550+Pablin89@users.noreply.github.com>
2023-09-21 21:05:28 -0300

    Update CajeroPrincipal.vb

--- Proyecto2023/CPresentacion/Cajero/CajeroPrincipal.vb
+++ Proyecto2023/CPresentacion/Cajero/CajeroPrincipal.vb
@@ -9,15 +9,12 @@
         Misdatos.ShowDialog()
     End Sub
 
-    Private Sub RealizarVenta_Click(sender As Object, e As EventArgs) Handles RealizarVenta.Click
-        Realizar_Venta.ShowDialog()
+    Private Sub ConsultarCliente_Click(sender As Object, e As EventArgs) Handles ConsultarCliente.Click
+        Seleccion_Clientes.ShowDialog()
+
     End Sub
 
     Private Sub AgregarUsuario_Click(sender As Object, e As EventArgs) Handles AgregarUsuario.Click
-        Gestion_Clientes.ShowDialog()
-    End Sub
-
-    Private Sub MisVentas_Click(sender As Object, e As EventArgs) Handles MisVentas.Click
-        Mis_ventas.ShowDialog()
+        AgregarCliente.ShowDialog()
     End Sub
 End Class
