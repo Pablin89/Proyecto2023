da2d7d6c437fe0cb1fd25571aed9fb935b70aff0
Diego David Almiron <diegodavidalmiron1990@gmail.com>
2023-10-04 09:54:45 -0300

    mis datos master

--- Proyecto2023/CPresentacion/Gerente/Gerente_Principal.vb
+++ Proyecto2023/CPresentacion/Gerente/Gerente_Principal.vb
@@ -26,6 +26,7 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Misdatos.BModificarP.Visible = False
         Misdatos.ShowDialog()
     End Sub
 End Class
