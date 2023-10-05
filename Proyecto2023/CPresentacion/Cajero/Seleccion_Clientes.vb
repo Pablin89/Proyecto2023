99457f1e3f91b8eb0ed9f811141ac1fff3a98632
Diego David Almiron <diegodavidalmiron1990@gmail.com>
2023-10-05 15:10:05 -0300

    busqueda en selección de clientes

--- Proyecto2023/CPresentacion/Cajero/Seleccion_Clientes.vb
+++ Proyecto2023/CPresentacion/Cajero/Seleccion_Clientes.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Net
 Imports System.Text.RegularExpressions
 
 Public Class Baja_Clientes
@@ -57,18 +58,21 @@ Public Class Baja_Clientes
                 MsgBox("Introduzca un DNI", MsgBoxStyle.Critical, "Error")
             Else
                 MsgBox("Seleccionaste buscar por DNI: " + dni, MsgBoxStyle.Information, "Buscar")
+                buscarClienteDni(dni)
             End If
         ElseIf (RBApellido.Checked) Then
             If (apellido = "") Then
                 MsgBox("Introduzca un apellido", MsgBoxStyle.Critical, "Error")
             Else
                 MsgBox("Seleccionaste buscar por Apellido: " + apellido, MsgBoxStyle.Information, "Buscar")
+                buscarClienteApellido(apellido)
             End If
         ElseIf (RBCorreo.Checked) Then
             If (correo = "") Then
                 MsgBox("Introduzca un correo", MsgBoxStyle.Critical, "Error")
             ElseIf (EmailAddressCheck(correo)) Then
                 MsgBox("Seleccionaste buscar por Correo: " + correo, MsgBoxStyle.Information, "Buscar")
+                buscarClienteCorreo(correo)
             Else
                 MsgBox("Formato de correo no válido. Formato permitido ej: 'usuario@gmail.com'", MsgBoxStyle.Critical, "Error")
             End If
@@ -84,6 +88,9 @@ Public Class Baja_Clientes
 
 
     Private Sub Baja_Clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TBuscarapellido.Enabled = False
+        TBuscarCorreo.Enabled = False
+        TBuscarDni.Enabled = False
         DataGridView1.AllowUserToAddRows = False
         conexion = New SqlConnection("server = DIEGO\SQLEXPRESS; database = Proyecto2023; integrated security = true")
         verClientes()
@@ -126,4 +133,106 @@ Public Class Baja_Clientes
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
         AgregarCliente.ShowDialog()
     End Sub
+
+    'busqueda diámica con dni
+    Public Sub buscarClienteDni(dni As String)
+        Try
+            Dim query As String = "select 
+                                id_cliente As id,
+                                apellido As Apellido,
+                                nombre As Nombre,
+                                telefono As Telefono,
+                                fecha_nacimiento As Nacimiento,
+                                correo As Mail,
+                                direccion As Dirección,
+                                dni As DNI
+                                from clientes where dni like '%" & dni & "%'"
+            Dim adaptador As New SqlDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            adaptador.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Public Sub buscarClienteApellido(apellido As String)
+        Try
+            Dim query As String = "select 
+                                id_cliente As id,
+                                apellido As Apellido,
+                                nombre As Nombre,
+                                telefono As Telefono,
+                                fecha_nacimiento As Nacimiento,
+                                correo As Mail,
+                                direccion As Dirección,
+                                dni As DNI
+                                from clientes where apellido like '%" & apellido & "%'"
+            Dim adaptador As New SqlDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            adaptador.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Public Sub buscarClienteCorreo(correo As String)
+        Try
+            Dim query As String = "select 
+                                id_cliente As id,
+                                apellido As Apellido,
+                                nombre As Nombre,
+                                telefono As Telefono,
+                                fecha_nacimiento As Nacimiento,
+                                correo As Mail,
+                                direccion As Dirección,
+                                dni As DNI
+                                from clientes where correo like '%" & correo & "%'"
+            Dim adaptador As New SqlDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            adaptador.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub RBDni_CheckedChanged(sender As Object, e As EventArgs) Handles RBDni.CheckedChanged
+        If (RBDni.Checked) Then
+            TBuscarapellido.Clear()
+            TBuscarapellido.Enabled = False
+            TBuscarCorreo.Clear()
+            TBuscarCorreo.Enabled = False
+            TBuscarDni.Enabled = True
+        End If
+    End Sub
+
+    Private Sub RBApellido_CheckedChanged(sender As Object, e As EventArgs) Handles RBApellido.CheckedChanged
+        If (RBApellido.Checked) Then
+
+            TBuscarapellido.Enabled = True
+            TBuscarCorreo.Enabled = False
+            TBuscarCorreo.Clear()
+            TBuscarDni.Enabled = False
+            TBuscarDni.Clear()
+        End If
+    End Sub
+
+    Private Sub RBCorreo_CheckedChanged(sender As Object, e As EventArgs) Handles RBCorreo.CheckedChanged
+        If (RBCorreo.Checked) Then
+
+            TBuscarDni.Clear()
+            RBApellido.Checked = False
+            TBuscarapellido.Clear()
+            TBuscarapellido.Enabled = False
+            TBuscarCorreo.Enabled = True
+            TBuscarDni.Enabled = False
+        End If
+    End Sub
+
 End Class
