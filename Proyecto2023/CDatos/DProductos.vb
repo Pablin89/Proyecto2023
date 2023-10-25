94c9345f2f0bc274e48af7561373e123b6ebc0cd
Diego David Almiron <diegodavidalmiron1990@gmail.com>
2023-10-25 10:15:18 -0300

    metodos insertarProducto

---
+++ Proyecto2023/CDatos/DProductos.vb
@@ -0,0 +1,133 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DProductos
+    Inherits Conexion
+    Private id_producto As Integer
+    Private descripcion As String
+    Private nombre As String
+    Private codigo As Integer
+    Private stock As Integer
+    Private strock_minimo As Integer
+    Private precio As Double
+    Private id_estado_producto As Integer
+    Private id_categoria As Integer
+    Private comando As SqlCommand
+    Private dr As SqlDataReader
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(desc As String, nom As String, cod As Integer, stk As Integer, stk_min As Integer, prec As Double, id_est As Integer, id_cat As Integer)
+        descripcion = desc
+        nombre = nom
+        codigo = cod
+        stock = stk
+        strock_minimo = stk_min
+        precio = prec
+        id_estado_producto = id_est
+        id_categoria = id_cat
+    End Sub
+
+    Property CIdProducto As String
+        Get
+            Return id_producto
+        End Get
+        Set(value As String)
+            id_producto = value
+        End Set
+    End Property
+
+    Property CDescripcion As String
+        Get
+            Return descripcion
+        End Get
+        Set(value As String)
+            descripcion = value
+        End Set
+    End Property
+
+    Property CNombre As String
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+            nombre = value
+        End Set
+    End Property
+
+    Property CCodigo As String
+        Get
+            Return codigo
+        End Get
+        Set(value As String)
+            codigo = value
+        End Set
+    End Property
+
+    Property CStock As String
+        Get
+            Return stock
+        End Get
+        Set(value As String)
+            stock = value
+        End Set
+    End Property
+
+    Property CStockMinimo As String
+        Get
+            Return strock_minimo
+        End Get
+        Set(value As String)
+            strock_minimo = value
+        End Set
+    End Property
+
+    Property CPrecio As String
+        Get
+            Return precio
+        End Get
+        Set(value As String)
+            precio = value
+        End Set
+    End Property
+
+    Property CIdEstadoProducto As String
+        Get
+            Return id_estado_producto
+        End Get
+        Set(value As String)
+            id_estado_producto = value
+        End Set
+    End Property
+
+    Property CIdCategoria As String
+        Get
+            Return id_categoria
+        End Get
+        Set(value As String)
+            id_categoria = value
+        End Set
+    End Property
+
+    Public Function insertarProducto(dproducto As DProductos) As Boolean
+        Try
+            conectar()
+
+            Dim query As String = "Insert into productos (descripcion,nombre,codigo,stock,stock_minimo,precio,id_estado_producto,id_categoria) values ('" & dproducto.descripcion & "','" & dproducto.CNombre & "','" & dproducto.CCodigo & "'," & dproducto.CStock & ",'" & dproducto.CStockMinimo & "'," & dproducto.CPrecio & ",'" & dproducto.CIdEstadoProducto & "'," & dproducto.CIdCategoria & ")"
+
+            comando = New SqlCommand(query, conexion)
+
+            If (comando.ExecuteNonQuery()) Then
+                Return True
+            Else
+                Return False
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+
+End Class
---
+++ Proyecto2023/CNegocio/NProductos.vb
@@ -0,0 +1,10 @@
+﻿Public Class NProductos
+    Public Function insertarProducto(descripcion As String, nombre As String, codigo As Integer, stock As Integer, stock_minimo As Integer, precio As Double, id_estado_producto As Integer, id_categoria As Integer) As Boolean
+        Dim dproducto As New DProductos(descripcion, nombre, codigo, stock, stock_minimo, precio, id_estado_producto, id_categoria)
+        If (dproducto.insertarProducto(dproducto)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
